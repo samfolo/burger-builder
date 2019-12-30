7d73d0aa92eb8d1a76cfa0d2b59a01e9d933fc9e
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-12-30 08:50:08 +0000

    create BurgerIngredient component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -5,12 +5,34 @@ const BurgerIngredient = props => {
   let ingredient;
 
   switch (props.type) {
-    case 'bread-bottom':
-      ingredient = <div className={Classes.BreadBottom}></div>
+    case ('bread-bottom'):
+      ingredient = <div className={Classes.BreadBottom}></div>;
       break;
+    case ('bread-top'):
+      ingredient = (
+        <div className={Classes.BreadTop}>
+          <div className={Classes.Seeds1}></div>
+          <div className={Classes.Seeds2}></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingredient = <div className={Classes.Meat}></div>
+      break;
+    case ('cheese'):
+      ingredient = <div className={Classes.Cheese}></div>
+      break;
+    case ('saladt'):
+      ingredient = <div className={Classes.Salad}></div>
+      break;
+    case ('bacon'):
+      ingredient = <div className={Classes.Bacon}></div>
+      break;
+    default:
+      ingredient = null;
   }
 
-  return ();
+  return ingredient;
 };
 
 export default BurgerIngredient;
