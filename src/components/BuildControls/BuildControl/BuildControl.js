4179b987ff5056e63c6121eb538360e7aac8c63f
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-12-30 10:03:06 +0000

    create BuildControl component

---
+++ src/components/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Classes from './BuildControl.module.css'
+
+const BuildControl = props => {
+  return (
+    <div className={Classes.BuildControl}>
+      <div className={Classes.Label}>{props.label}</div>
+      <button className={Classes.Less}>Less</button>
+      <button className={Classes.More}>More</button>
+    </div>
+  );
+}
+
+export default BuildControl;
---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const BuildControls = props => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default BuildControls;
