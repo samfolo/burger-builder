3cbed314438087e7f1f112dc94ea4b07b2077aab
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-04 12:04:57 +0000

    set up redux devtools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import orderReducer from './store/reducers/orderReducer';
 import './index.css';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   builder: orderReducer,
@@ -23,7 +25,9 @@ const logger = store => {
   }
 }
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(logger)
+));
 
 const app = (
   <BrowserRouter>
