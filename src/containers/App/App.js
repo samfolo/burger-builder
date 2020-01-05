7cc5b89ab4a86863acf1a0db123731c0fd973b96
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-05 22:42:26 +0000

    guard routes by conditional rendering and redirecting

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Classes from './App.module.css';
 import Layout from '../../hoc/Layout/Layout';
@@ -11,20 +12,45 @@ import Logout from '../Logout/Logout';
 
 class App extends React.Component {
   render() {
+    let routes = (
+      <Switch>
+        <Route path='/' exact component={BurgerBuilder} />
+        <Route path='/auth' component={Auth} />
+        <Redirect to='/' />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/' exact component={BurgerBuilder} />
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/auth' component={Auth} />
+          <Route path='/sign-out' component={Logout} />
+        </Switch>
+      );
+    }
+
     return (
       <div className={Classes.App}>
         <Layout>
-          <Switch>
-            <Route path='/' exact component={BurgerBuilder} />
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path='/auth' component={Auth} />
-            <Route path='/sign-out' component={Logout} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.loggedIn,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
