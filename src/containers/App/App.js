c661d979faeecf6d97ceda736f12bb2440138b81
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-05 11:01:23 +0000

    add an authentication form

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,6 +6,7 @@ import Layout from '../../hoc/Layout/Layout';
 import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
 import Checkout from '../Checkout/Checkout';
 import Orders from '../Orders/Orders';
+import Auth from '../Auth/Auth';
 
 class App extends React.Component {
   render() {
@@ -16,6 +17,7 @@ class App extends React.Component {
             <Route path='/' exact component={BurgerBuilder} />
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' component={Orders} />
+            <Route path='/auth' component={Auth} />
           </Switch>
         </Layout>
       </div>
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import Classes from './Auth.module.css';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends React.Component {
+  state ={
+    authForm: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Enter your email',
+        },
+        value: '',
+        validation: {
+          required: true,
+          // eslint-disable-next-line no-useless-escape
+          format: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        },
+        valid: false,
+        touched: false,
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Enter your password',
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+    }
+  }
+
+  checkValidity = (fieldValue, rules) => {
+    let isValid = true ;
+    let trueFieldValue = fieldValue.trim();
+
+    if (rules.required) { isValid = isValid && trueFieldValue !== ''; }
+    if (rules.minLength) { isValid = isValid && trueFieldValue.length >= rules.minLength }
+    if (rules.maxLength) { isValid = isValid && trueFieldValue.length <= rules.maxLength }
+    if (rules.format) { isValid = isValid && rules.format.test(trueFieldValue); }
+
+    return isValid
+  }
+
+  renderInputs = () => {
+    const authFormKeys = Object.keys(this.state.authForm);
+
+    return authFormKeys.map((key, i) => {
+      return <Input 
+        key={`${i}_${key}`}
+        elementType={this.state.authForm[key].elementType} 
+        elementConfig={this.state.authForm[key].elementConfig} 
+        value={this.state.authForm[key].value}
+        validation={this.state.authForm[key].validation}
+        valid={this.state.authForm[key].valid}
+        shouldValidate={this.state.authForm[key].validation ? true : undefined}
+        touched={this.state.authForm[key].touched}
+        onChange={(e) => this.handleChange(e, key)} />
+    });
+  }
+
+  handleChange = (e, field) => {
+    const authForm = {...this.state.authForm};
+    const authFormField = {...authForm[field]};
+    authFormField.value = e.target.value;
+    authFormField.valid = this.checkValidity(authFormField.value, authFormField.validation);
+    authFormField.touched = true;
+    authForm[field] = authFormField; // above is deep cloning
+
+    this.setState({ authForm: authForm })
+  }
+  
+  isInvalidForm = () => {
+    const allFields = Object.keys(this.state.authForm);
+    const validFields = allFields.map(field => {
+      return (
+        this.state.authForm[field].valid
+      );
+    });
+    return validFields.includes(false);
+  }
+
+  render() {
+    const form = this.renderInputs();
+
+    return (
+      <div className={Classes.Auth}>
+        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {form}
+          <Button buttonType='Success' disabled={this.isInvalidForm()}>SUBMIT</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
