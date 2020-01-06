141d7bd9de1008189ee13f263caed357216de445
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-06 08:58:41 +0000

    use asyncComponents to lazy load components on some routes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import asyncComponent from '../../hoc/asyncComponents';
 
 import Classes from './App.module.css';
 import Layout from '../../hoc/Layout/Layout';
 import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
-import Checkout from '../Checkout/Checkout';
-import Orders from '../Orders/Orders';
-import Auth from '../Auth/Auth';
 import Logout from '../Logout/Logout';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('../Checkout/Checkout');
+})
+
+const asyncOrders = asyncComponent(() => {
+  return import('../Orders/Orders');
+})
+
+const asyncAuth = asyncComponent(() => {
+  return import('../Auth/Auth');
+})
 
 class App extends React.Component {
   render() {
     let routes = (
       <Switch>
         <Route path='/' exact component={BurgerBuilder} />
-        <Route path='/auth' component={Auth} />
+        <Route path='/auth' component={asyncAuth} />
         <Redirect to='/' />
       </Switch>
     )
@@ -24,9 +34,9 @@ class App extends React.Component {
       routes = (
         <Switch>
           <Route path='/' exact component={BurgerBuilder} />
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' component={asyncOrders} />
+          <Route path='/auth' component={asyncAuth} />
           <Route path='/sign-out' component={Logout} />
         </Switch>
       );
